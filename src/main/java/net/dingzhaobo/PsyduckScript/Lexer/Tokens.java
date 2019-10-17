14fc6daba74dea4610f925e38e36f5e48562ea7a
BananaWolfDing <ding.zb@yahoo.com>
2019-10-16 20:08:04 -0400

    created tokens

---
+++ src/main/java/net/dingzhaobo/PsyduckScript/Lexer/Tokens.java
@@ -0,0 +1,57 @@
+package net.dingzhaobo.PsyduckScript.Lexer;
+
+public enum Tokens {
+    INVALID,   // invalid
+
+    ID,        // identifier
+    EOF,       // end of file
+
+    INT,       // integer
+    DOUBLE,    // double
+    STRING,    // string
+
+    PLUS,      // +
+    MINUS,     // -
+    MULT,     // *
+    DIV,    // /
+    MOD,       // %
+
+    AND,       // &&
+    OR,        // ||
+    NOT,       // !
+    BITXOR,    // ^
+    BITAND,    // &
+    BITOR,     // |
+    BITNOT,    // !
+
+    EQ,        // ==
+    NE,        // !=
+    GT,        // >
+    LT,        // <
+    GE,        // >=
+    LE,        // <=
+
+    AGN,       // =
+    PLUSAGN,   // +=
+    MINUSAGN,  // -=
+    MULTAGN,  // *=
+    DIVAGN, // /=
+    MODAGN,    // %=
+
+    COMMA,     // ,
+    LPAREN,    // (
+    RPAREN,    // )
+    LBRACE,    // {
+    RBRACE,    // }
+    LBRACKET,  // [
+    RBRACKET,  // ]
+
+    IF,        // if
+    ELSE,      // else
+    WHILE,     // while
+    FUNC,      // function
+    RETURN,    // return
+    TRUE,      // True
+    FALSE,     // False
+    NONE       // NoneVal
+}
